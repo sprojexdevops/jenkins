809fb363b73f0f0c8321cda9f245e9d35074d5fc
yeswanth <yswnth.2010@gmail.com>
2024-11-29 16:56:00 +0530

    jenkins practice

--- expense-infra-tf/20-bastion/bastion.sh
+++ expense-infra-tf/20-bastion/bastion.sh
@@ -30,6 +30,10 @@ else
     echo "You are super user."
 fi
 
+# mysql client
+dnf install -y mysql
+VALIDATE $? "mysql installation"
+
 # docker
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
